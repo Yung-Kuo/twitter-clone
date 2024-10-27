95ddc966c6fa2a9939d0864018eb9340890d4982
Yung-Kuo <yungkuo429@gmail.com>
2024-10-27 22:04:20 +0800

    add facebook logo and login button

---
+++ components/Login/Facebook.vue
@@ -0,0 +1,22 @@
+<script setup>
+const config = useRuntimeConfig();
+const supabase = useSupabaseClient();
+async function signInWithFacebook() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "facebook",
+    options: {
+      redirectTo: `${config.public.baseURL}/confirm`,
+    },
+  });
+  if (error) console.log(error);
+}
+</script>
+<template>
+  <UIButton3DGlow
+    class="h-12 w-72 md:h-14 md:w-80"
+    @mousedown="signInWithFacebook()"
+  >
+    <IconsFacebook />
+    Log in with Facebook
+  </UIButton3DGlow>
+</template>
